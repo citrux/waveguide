07adf8fba1771b07f842b382d0162a04a2efa736
Vova Abdrakhmanov <369565@gmail.com>
2013-12-21 13:09:25 +0400

    both types of waves in physics.py and plot transversal field with plots.py

--- physics.py
+++ physics.py
@@ -1,23 +1,32 @@
-# coding: utf-8
-###############################################################################
-# графическое решение дисперсионного уравнения
-# совместимо и с python 2, и с python 3
-# частоты СВЧ 3 ÷ 30 ГГц → omega = 20 ÷ 200 Град/с
-###############################################################################
-
-DEBUG = False
-
 import numpy as np
-from math import pi, tan, log
 import matplotlib.pyplot as plt
 from settings import *
 
-def e_relation(u1, u2):
-    return u1 * tan(u1 * l1) / e1 + u2 * tan(u2 * l2) / e2
+
+def lm_dispersion_relation(u1_sqr, u2_sqr):
+    '''
+        Дисперсионное соотношение для LM-волн
+        Если u_1^2 < 0, то волна экспонентцциально затухает в первой области
+    '''
+    if u1_sqr < 0:
+        u1, u2 = sqrt(-u1_sqr), sqrt(u2_sqr)
+        return -u1 * np.tanh(u1 * l1) / e1 + u2 * np.tan(u2 * l2) / e2
+    else:
+        u1, u2 = sqrt(u1_sqr), sqrt(u2_sqr)
+        return u1 * np.tan(u1 * l1) / e1 + u2 * np.tan(u2 * l2) / e2
 
 
-def m_relation(u1, u2):
-    return m1 * tan(u1 * l1) / u1 + m2 * tan(u2 * l2) / u2
+def le_dispersion_relation(u1_sqr, u2_sqr):
+    '''
+        Дисперсионное соотношение для LE-волн
+        Если u_1^2 < 0, то волна экспонентцциально затухает в первой области
+    '''
+    if u1_sqr < 0:
+        u1, u2 = sqrt(-u1_sqr), sqrt(u2_sqr)
+        return m1 * np.tanh(u1 * l1) / u1 + m2 * np.tan(u2 * l2) / u2
+    else:
+        u1, u2 = sqrt(u1_sqr), sqrt(u2_sqr)
+        return m1 * np.tan(u1 * l1) / u1 + m2 * np.tan(u2 * l2) / u2
 
 
 def bisection(f, left, right, precision):
@@ -119,7 +128,7 @@ def transversal_data(relation, m_list, omega_list, precision):
 
     for omega in omega_list:
         # рисуем гиперболу для частоты
-        n = 0 if relation is m_relation else 1
+        n = 0 if relation is le_dispersion_relation else 1
         border = (omega/sol)**2 * e1 * m1 - (pi * n / b) ** 2
         if border < 0:
             border = 0
@@ -170,99 +179,4 @@ def longitudinal_data(relation, m_list, n_list, omega_list, precision):
                     o_list.append(omega)
             result["curves"].append((o_list, h_list));
     return result
-
-def plot_transversal_field(relation, m, n, omega):
-    u1, u2 = transversal_wavenumbers(relation, m, omega, 1e-3)
-    h = ((omega/sol)**2 * e1 * m1 - u1 **2 - (pi*n/b) ** 2) ** 0.5
-    Y1, X1 = np.mgrid[0:b:91j, c:a:106j]
-    Y2, X2 = np.mgrid[0:b:91j, 0:c:46j]
-    if relation is e_relation:
-        family = "e"
-        # левая область (2): от 0 до с
-        E2x = -(h ** 2 + (pi * n / b) ** 2) / u2 / h *\
-            np.sin (u1 * (c-a)) / np.sin(u2 * c) * \
-            np.cos(u2*X2) * np.sin(pi * n / b * Y2)
-        E2y = pi * n / h / b * np.sin (u1 * (c-a)) / np.sin(u2 * c) *\
-            np.sin(u2*X2) * np.cos(pi * n / b * Y2)
-        E2xy = np.sqrt(E2x*E2x + E2y*E2y)
-
-        H2x =  0 * X2
-        H2y = -e0 * e2 * omega / u2 * np.sin (u1 * (c-a)) / np.sin(u2 * c) * \
-            np.cos(u2*X2) * np.sin(pi * n / b * Y2)
-        H2xy = np.sqrt(H2x*H2x + H2y*H2y)
-
-        # правая область (1): от c до a
-        E1x = -(h ** 2 + (pi * n / b) ** 2) / u1 / h *\
-            np.cos(u1*(X1 - a)) * np.sin(pi * n / b * Y1)
-        E1y = pi * n / h / b * np.sin(u1*(X1 - a)) * np.cos(pi * n / b * Y1)
-        E1xy = np.sqrt(E1x*E1x + E1y*E1y)
-
-        H1x =  0 * X1
-        H1y = -e0 * e1 * omega / u1 *\
-            np.cos(u1*(X1 - a)) * np.sin(pi * n / b * Y1)
-        H1xy = np.sqrt(H1x*H1x + H1y*H1y)
-    
-    elif relation is m_relation:
-        family = "m"
-        # левая область (2): от 0 до с
-        E2x = 0 * X2
-        E2y = -omega * m0 * m2 / u2 * np.cos(u1 * (c-a)) / np.cos(u2 * c) *\
-            np.sin(u2*X2) * np.cos(pi * n / b * Y2)
-        E2xy = np.sqrt(E2x*E2x + E2y*E2y)
-
-        H2x =  (h ** 2 + (pi * n / b) ** 2) / u2 / h *\
-            np.cos(u1 * (c-a)) / np.cos(u2 * c) *\
-            np.sin(u2*X2) * np.cos(pi * n / b * Y2)
-        H2y = -pi * n / h / b * np.cos(u1 * (c-a)) / np.cos(u2 * c) *\
-            np.cos(u2*X2) * np.sin(pi * n / b * Y2)
-        H2xy = np.sqrt(H2x*H2x + H2y*H2y)
-
-        # правая область (1): от c до a
-        E1x = 0 * X1
-        E1y = -omega * m0 * m2 / u1 * np.sin(u1*(X1 - a)) * np.cos(pi * n / b * Y1)
-        E1xy = np.sqrt(E1x*E1x + E1y*E1y)
-
-        H1x =  (h ** 2 + (pi * n / b) ** 2) / u1 / h *\
-            np.sin(u1*(X1 - a)) * np.cos(pi * n / b * Y1)
-        H1y = -pi * n / h / b * np.cos(u1*(X1 - a)) * np.sin(pi * n / b * Y1)
-        H1xy = np.sqrt(H1x*H1x + H1y*H1y)
-
-
-    Emax = max(E1xy.max(), E2xy.max())
-    Hmax = max(H1xy.max(), H2xy.max())
-
-    lw = 1.5 * E2xy / Emax
-    plt.streamplot(X2, Y2, E2x, E2y, density=.4, color="k", linewidth=lw)
-
-    lw = 1.5 * H2xy / Hmax
-    plt.streamplot(X2, Y2, H2x, H2y, density=.4, color="b", linewidth=lw)
-
-    lw = 1.5 * E1xy / Emax
-    plt.streamplot(X1, Y1, E1x, E1y, density=.7, color="k", linewidth=lw)
-
-    lw = 1.5 * H1xy / Hmax
-    plt.streamplot(X1, Y1, H1x, H1y, density=.7, color="b", linewidth=lw)
-    plt.tick_params(
-        axis='both',          # changes apply to both-axises
-        which='both',      # both major and minor ticks are affected
-        bottom='off',      # ticks along the bottom edge are off
-        top='off',         # ticks along the top edge are off
-        left='off',
-        right='off',
-        labelbottom='off',
-        labeltop='off',
-        labelleft='off',
-        labelright='off') # labels along the bottom edge are off
-    plt.plot([c,c], [0,b], "k-")
-    plt.title("$%s_{%d%d},\ %.1e\ rad/s$" %(family, m, n, omega))
-    plt.savefig("field_%s_%d_%d_%.1e.pdf" %(family, m, n, omega))
-    plt.cla()
-
-
-if __name__ == '__main__':
-    plot_transversal_field(m_relation, 1, 0, 1.6e10)
-    plot_transversal_field(m_relation, 2, 0, 4e10)
-    plot_transversal_field(e_relation, 2, 1, 4.5e10)
-    plot_transversal_field(m_relation, 2, 1, 4.2e10)
-    plot_transversal_field(m_relation, 3, 0, 5e10)
 
--- plots.py
+++ plots.py
@@ -1,3 +1,6 @@
+from physics import lm_dispersion_relation, le_dispersion_relation
+
+
 def plot_transversal(relation, m_list, omega_list, precision):
     rel = hw.e_relation if relation=="e" else hw.m_relation
     data = hw.transversal_data(rel, m_list, omega_list, precision)
@@ -87,4 +90,173 @@ def plot_longitudinal(m_list, n_list, omega_list, precision):
     save_file(plt, "dispersion.pdf")
     plt.cla()
 
+
+def plot_transversal_field(relation, m, n, omega, border_orientation="|"):
+    '''
+    Строит картину поля в поперечном сечении
+    border_orientation -- ориентация границы раздела, может принимать значения
+    "|" и "-"
+    '''
+    # формируем набор точек для построения полей в областях
+    Y1, X1 = np.mgrid[0:b:91j, c:a:106j]
+    Y2, X2 = np.mgrid[0:b:91j, 0:c:46j]
+
+    # определяем поперечные и продольные волновые числа
+    u1_sqr, u2_sqr = transversal_wavenumbers(relation, m, omega, 1e-3)
+    h = ((omega/sol)**2 * e1 * m1 - u1_sqr - (pi*n/b) ** 2) ** 0.5
+
+    if u1_sqr < 0:
+        # гиперболические волны
+        u1, u2 = np.sqrt(-u1_sqr), np.sqrt(u2_sqr)
+        if relation is lm_dispersion_relation:
+            family = "lm"
+            # левая область (2): от 0 до с
+            E2x = -(h ** 2 + (pi * n / b) ** 2) / u2 / h *\
+                np.sinh (u1 * (c-a)) / np.sin(u2 * c) * \
+                np.cos(u2*X2) * np.sin(pi * n / b * Y2)
+            E2y = pi * n / h / b * np.sinh (u1 * (c-a)) / np.sin(u2 * c) *\
+                np.sin(u2*X2) * np.cos(pi * n / b * Y2)
+            E2xy = np.sqrt(E2x*E2x + E2y*E2y)
+
+            H2x =  0 * X2
+            H2y = -e0 * e2 * omega / u2 * np.sinh (u1 * (c-a)) / np.sin(u2 * c) * \
+                np.cos(u2*X2) * np.sin(pi * n / b * Y2)
+            H2xy = np.sqrt(H2x*H2x + H2y*H2y)
+
+            # правая область (1): от c до a
+            E1x = (h ** 2 + (pi * n / b) ** 2) / u1 / h *\
+                np.cosh(u1*(X1 - a)) * np.sin(pi * n / b * Y1)
+            E1y = pi * n / h / b * np.sinh(u1*(X1 - a)) * np.cos(pi * n / b * Y1)
+            E1xy = np.sqrt(E1x*E1x + E1y*E1y)
+
+            H1x =  0 * X1
+            H1y = e0 * e1 * omega / u1 *\
+                np.cosh(u1*(X1 - a)) * np.sin(pi * n / b * Y1)
+            H1xy = np.sqrt(H1x*H1x + H1y*H1y)
+
+        elif relation is le_dispersion_relation:
+            family = "le"
+            # левая область (2): от 0 до с
+            E2x = 0 * X2
+            E2y = -omega * m0 * m2 / u2 * np.cosh(u1 * (c-a)) / np.cos(u2 * c) *\
+                np.sin(u2*X2) * np.cos(pi * n / b * Y2)
+            E2xy = np.sqrt(E2x*E2x + E2y*E2y)
+
+            H2x = (h ** 2 + (pi * n / b) ** 2) / u2 / h *\
+                np.cosh(u1 * (c-a)) / np.cos(u2 * c) *\
+                np.sin(u2*X2) * np.cos(pi * n / b * Y2)
+            H2y = -pi * n / h / b * np.cosh(u1 * (c-a)) / np.cos(u2 * c) *\
+                np.cos(u2*X2) * np.sin(pi * n / b * Y2)
+            H2xy = np.sqrt(H2x*H2x + H2y*H2y)
+
+            # правая область (1): от c до a
+            E1x = 0 * X1
+            E1y = -omega * m0 * m2 / u1 * np.sinh(u1*(X1 - a)) *\
+                np.cos(pi * n / b * Y1)
+            E1xy = np.sqrt(E1x*E1x + E1y*E1y)
+
+            H1x =  (h ** 2 + (pi * n / b) ** 2) / u1 / h *\
+                np.sinh(u1*(X1 - a)) * np.cos(pi * n / b * Y1)
+            H1y = -pi * n / h / b * np.cosh(u1*(X1 - a)) * np.sin(pi * n / b * Y1)
+            H1xy = np.sqrt(H1x*H1x + H1y*H1y)
+
+    else:
+        # гармонические волны
+        u1, u2 = np.sqrt(u1_sqr), np.sqrt(u2_sqr)
+        if relation is lm_dispersion_relation:
+            family = "lm"
+            # левая область (2): от 0 до с
+            E2x = -(h ** 2 + (pi * n / b) ** 2) / u2 / h *\
+                np.sin (u1 * (c-a)) / np.sin(u2 * c) * \
+                np.cos(u2*X2) * np.sin(pi * n / b * Y2)
+            E2y = pi * n / h / b * np.sin (u1 * (c-a)) / np.sin(u2 * c) *\
+                np.sin(u2*X2) * np.cos(pi * n / b * Y2)
+            E2xy = np.sqrt(E2x*E2x + E2y*E2y)
+
+            H2x =  0 * X2
+            H2y = -e0 * e2 * omega / u2 * np.sin (u1 * (c-a)) / np.sin(u2 * c) * \
+                np.cos(u2*X2) * np.sin(pi * n / b * Y2)
+            H2xy = np.sqrt(H2x*H2x + H2y*H2y)
+
+            # правая область (1): от c до a
+            E1x = -(h ** 2 + (pi * n / b) ** 2) / u1 / h *\
+                np.cos(u1*(X1 - a)) * np.sin(pi * n / b * Y1)
+            E1y = pi * n / h / b * np.sin(u1*(X1 - a)) * np.cos(pi * n / b * Y1)
+            E1xy = np.sqrt(E1x*E1x + E1y*E1y)
+
+            H1x =  0 * X1
+            H1y = -e0 * e1 * omega / u1 *\
+                np.cos(u1*(X1 - a)) * np.sin(pi * n / b * Y1)
+            H1xy = np.sqrt(H1x*H1x + H1y*H1y)
+
+        elif relation is le_dispersion_relation:
+            family = "le"
+            # левая область (2): от 0 до с
+            E2x = 0 * X2
+            E2y = -omega * m0 * m2 / u2 * np.cos(u1 * (c-a)) / np.cos(u2 * c) *\
+                np.sin(u2*X2) * np.cos(pi * n / b * Y2)
+            E2xy = np.sqrt(E2x*E2x + E2y*E2y)
+
+            H2x =  (h ** 2 + (pi * n / b) ** 2) / u2 / h *\
+                np.cos(u1 * (c-a)) / np.cos(u2 * c) *\
+                np.sin(u2*X2) * np.cos(pi * n / b * Y2)
+            H2y = -pi * n / h / b * np.cos(u1 * (c-a)) / np.cos(u2 * c) *\
+                np.cos(u2*X2) * np.sin(pi * n / b * Y2)
+            H2xy = np.sqrt(H2x*H2x + H2y*H2y)
+
+            # правая область (1): от c до a
+            E1x = 0 * X1
+            E1y = -omega * m0 * m2 / u1 * np.sin(u1*(X1 - a)) * np.cos(pi * n / b * Y1)
+            E1xy = np.sqrt(E1x*E1x + E1y*E1y)
+
+            H1x =  (h ** 2 + (pi * n / b) ** 2) / u1 / h *\
+                np.sin(u1*(X1 - a)) * np.cos(pi * n / b * Y1)
+            H1y = -pi * n / h / b * np.cos(u1*(X1 - a)) * np.sin(pi * n / b * Y1)
+            H1xy = np.sqrt(H1x*H1x + H1y*H1y)
+
+
+    Emax = max(E1xy.max(), E2xy.max())
+    Hmax = max(H1xy.max(), H2xy.max())
+
+    if border_orientation == "|":
+            lw = 1.5 * E2xy / Emax
+            plt.streamplot(X2, Y2, E2x, E2y, density=.4, color="k", linewidth=lw)
+
+            lw = 1.5 * H2xy / Hmax
+            plt.streamplot(X2, Y2, H2x, H2y, density=.4, color="b", linewidth=lw)
+
+            lw = 1.5 * E1xy / Emax
+            plt.streamplot(X1, Y1, E1x, E1y, density=.7, color="k", linewidth=lw)
+
+            lw = 1.5 * H1xy / Hmax
+            plt.streamplot(X1, Y1, H1x, H1y, density=.7, color="b", linewidth=lw)
+
+    elif border_orientation == "-":
+            lw = 1.5 * E2xy / Emax
+            plt.streamplot(Y2, X2, E2y, E2x, density=.4, color="k", linewidth=lw)
+
+            lw = 1.5 * H2xy / Hmax
+            plt.streamplot(Y2, X2, H2y, H2x, density=.4, color="b", linewidth=lw)
+
+            lw = 1.5 * E1xy / Emax
+            plt.streamplot(Y1, X1, E1y, E1x, density=.7, color="k", linewidth=lw)
+
+            lw = 1.5 * H1xy / Hmax
+            plt.streamplot(Y1, X1, H1y, H1x, density=.7, color="b", linewidth=lw)
+
+    plt.tick_params(
+        axis='both',          # changes apply to both-axises
+        which='both',      # both major and minor ticks are affected
+        bottom='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        left='off',
+        right='off',
+        labelbottom='off',
+        labeltop='off',
+        labelleft='off',
+        labelright='off') # labels along the bottom edge are off
+    plt.plot([c,c], [0,b], "k-")
+    plt.title("$%s_{%d%d},\ %.1e\ rad/s$" %(family, m, n, omega))
+    plt.savefig("field_%s_%d_%d_%.1e.pdf" %(family, m, n, omega))
+    plt.cla()
 
